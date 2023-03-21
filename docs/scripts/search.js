aeea043d40fb3cd2ea725f65c365a980100cbd33
matiasvlevi <matiasvlevi@gmail.com>
2023-03-21 17:36:24 -0400

    docs

--- docs/scripts/search.js
+++ docs/scripts/search.js
@@ -108,16 +108,17 @@ function buildSearchResult(result) {
   const removeHTMLTagsRegExp = /(<([^>]+)>)/ig;
   
   for (const res of result) {
-    const { title = '', description = '' } = res.item;
+    const { title = '', description = '', summary = '' } = res.item;
 
     const _link = res.item.link.replace('<a href="', '').replace(/">.*/, '');
     const _title = title.replace(removeHTMLTagsRegExp, "");
     const _description = description.replace(removeHTMLTagsRegExp, "");
+    const _summary = summary.replace(removeHTMLTagsRegExp, "");
 
     output += `
     <a onclick="onClickSearchItem(event)" href="${_link}" class="search-result-item">
       <div class="search-result-item-title">${_title}</div>
-      <div class="search-result-item-p">${_description || 'No description available.'}</div>
+      <div class="search-result-item-p">${_description || _summary || 'No description or summary available.'}</div>
     </a>
     `;
   }
@@ -137,7 +138,6 @@ function getSearchResult(list, keys, searchKey) {
   };
 
   const options = { ...defaultOptions };
-
   // eslint-disable-next-line no-undef
   const searchIndex = Fuse.createIndex(options.keys, list);
 
@@ -179,7 +179,7 @@ let searchData;
 
 async function search(event) {
   const value = event.target.value;
-  const keys = ['title', 'description'];
+  const keys = ['title', 'description', 'summary'];
 
   if (!resultBox) {
     console.error('Search result container not found');
@@ -195,7 +195,6 @@ async function search(event) {
 
   if (!searchData) {
     showResultText('Loading...');
-
     try {
       // eslint-disable-next-line require-atomic-updates
       searchData = await fetchAllData();
